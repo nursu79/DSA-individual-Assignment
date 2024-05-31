85d2588374ef737d6d573e5d893a867d7495f1ce
sumeya Ibrahim <167539336+nursu79@users.noreply.github.com>
2024-05-31 12:07:01 +0300

    Add files via upload

---
+++ IndividualASG/IndividualP2/src/Stack.java
@@ -0,0 +1,32 @@
+
+
+public class Stack {
+
+
+
+    Node head;
+
+    void push(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    int pop() {
+        if (head == null) {
+            return -1;
+        }
+        int data = head.data;
+        head = head.next;
+        return data;
+    }
+
+    int peek() {
+        if (head == null) {
+            return -1;
+        }
+        return head.data;
+    
+
+}
+}
